ca649a8c95581893eebc1b5eee6971d5bb332c83
paulomoncaio <paulo.moncaio@gmail.com>
2024-02-29 23:08:49 -0300

    add Header component

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import avatarUsuario from './avatar.svg'
+import Image from 'next/image'
+
+export default function Header() {
+  return (
+    <header className="flex h-[76px] items-center justify-center bg-[#111111]">
+      <div className="mx-auto flex w-[1199px] justify-between">
+        <Image src={'./bytebank.svg'} alt={'logo'} />
+        <div className="flex w-[282px] items-center justify-center">
+          <p className="mr-6 text-sm text-[#31b237]">Joana Fonseca Gomes</p>
+          <Image
+            className="size-10"
+            src={avatarUsuario}
+            alt="Ícone de um avatar de usuário"
+          />
+        </div>
+      </div>
+    </header>
+  )
+}
