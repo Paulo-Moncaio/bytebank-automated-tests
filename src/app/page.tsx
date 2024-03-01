4b543c3ea626b0d925a024c2d71a01a1d183a20b
paulomoncaio <paulo.moncaio@gmail.com>
2024-02-29 23:10:07 -0300

    Home page showing header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,30 @@
+import React, { useState } from 'react'
+import { calculateNewBalance } from '@/utils/calculateNewBalance'
+import { Header } from './components'
+
+type Transacao = { transacao: string; valor: string }
+
 export default function Home() {
-  return <main>App</main>
+  const [saldo, setSaldo] = useState(1000)
+  const [transacoes, setTransacoes] = useState<Transacao[]>([])
+
+  function handleTransaction(valores: Transacao) {
+    const novoSaldo = calculateNewBalance(valores, saldo)
+    setSaldo(novoSaldo)
+    setTransacoes([...transacoes, valores])
+  }
+
+  return (
+    <>
+      <Header />
+      {/* <main className={estilos.container}>
+        <Menu />
+        <div className={estilos.wrapper}>
+          <Principal saldo={saldo} />
+          <Transacao realizarTransacao={realizarTransacao} />
+        </div>
+        <Extrato transacoes={transacoes} />
+      </main> */}
+    </>
+  )
 }
