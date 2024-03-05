c85972f712dd3ff2bd08cb2cc6980b5347329d61
Paulo-Moncaio <paulomoncaio@gmail.com>
2024-03-04 21:52:50 -0300

    fix logo svg import

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import avatarUsuario from './avatar.svg'
+import bytebankLogo from './bytebank.svg'
 import Image from 'next/image'
 
 export default function Header() {
   return (
     <header className="flex h-[76px] items-center justify-center bg-[#111111]">
       <div className="mx-auto flex w-[1199px] justify-between">
-        <Image src={'./bytebank.svg'} alt={'logo'} />
+        <Image src={bytebankLogo} alt={'logo'} />
         <div className="flex w-[282px] items-center justify-center">
           <p className="mr-6 text-sm text-[#31b237]">Joana Fonseca Gomes</p>
           <Image
