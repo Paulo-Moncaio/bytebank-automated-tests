dc5d0da71b8c6f3971033ef4c4f35ece5f25be52
paulomoncaio <paulo.moncaio@gmail.com>
2024-03-04 21:53:40 -0300

    feature statement and transaction component

---
+++ src/app/components/Statement/Statement.tsx
@@ -0,0 +1,23 @@
+import Transaction from './Transaction/Transaction'
+
+type StatementProps = {
+  transactions: {
+    type: string
+    month: string
+    date: string
+    amount: number
+  }[]
+}
+
+export default function Statement({ transactions }: StatementProps) {
+  return (
+    <section className="w-[282px] overflow-hidden rounded-lg bg-[#f7f7f7] p-6 shadow-lg">
+      <h2 className="mb-6 text-xl font-extrabold text-[#111111]">Extrato</h2>
+      <ul>
+        {transactions.map((transaction, index) => (
+          <Transaction key={index} transaction={transaction} />
+        ))}
+      </ul>
+    </section>
+  )
+}
