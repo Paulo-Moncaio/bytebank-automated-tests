0490e51e030201d5f9cff01b87779eef6d8daf6e
Paulo-Moncaio <paulomoncaio@gmail.com>
2024-03-04 21:53:40 -0300

    feature statement and transaction component

---
+++ src/app/components/Statement/Transaction/Transaction.tsx
@@ -0,0 +1,32 @@
+export type TransactionProps = {
+  transaction: {
+    type: string
+    month: string
+    date: string
+    amount: number
+  }
+}
+
+export default function Transaction({ transaction }: TransactionProps) {
+  const isTransfer = transaction.type === 'Transferência'
+  const amountPrefix = isTransfer ? '- R$' : 'R$'
+
+  return (
+    <li>
+      <p className="mb-6 text-xs font-extrabold text-[#111111]">
+        {transaction.month}
+      </p>
+      <div className="my-2 flex w-full justify-between text-left">
+        <p className="text-sm text-black" data-testid="tipoTransacao">
+          {transaction.type}
+        </p>
+        <span className="text-xs text-[#8b8b8b]">{transaction.date}</span>
+      </div>
+      <h3
+        data-testid="valorTransacao"
+        className=""
+      >{`${amountPrefix}  ${transaction.amount}`}</h3>
+      <div className="mx-auto my-3 h-[1px] w-full bg-[##47a13880]" />
+    </li>
+  )
+}
