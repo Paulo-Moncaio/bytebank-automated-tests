e0ae5c590528e82eea517ec93ecac93374a7c666
paulomoncaio <paulo.moncaio@gmail.com>
2024-03-07 11:56:04 -0300

    configuring jest with typescript

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const nextJest = require('next/jest.js')
+const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
@@ -7,11 +7,13 @@ const createJestConfig = nextJest({
 })
 
 // Add any custom config to be passed to Jest
+/** @type {import("jest").Config} */
 const config = {
-  coverageProvider: 'v8',
+  preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  coverageProvider: 'v8',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
